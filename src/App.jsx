44e0abfe3d2802ae6f3107b6df1b0957ce2ae57c
aaroj <aaro.jylhamaki@outlook.com>
2025-04-08 10:27:12 +0300

    Add routing and restructure application layout with new views
    - Implemented routing using react-router for navigation between Home, Profile, and Upload views.
    - Created a new Layout component to manage the application structure.
    - Moved Home component to views directory and updated its implementation.
    - Added Profile, Upload, and Single components with basic structure.
    - Removed old Home component from components directory.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
-import Home from './components/Home';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Upload from './views/Upload';
+import {Route, BrowserRouter as Router, Routes} from 'react-router';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <>
-      <h1>My App</h1>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/upload" element={<Upload />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
